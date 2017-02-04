2df59e2fa4a115ecb06fc6d568de19da3f9d7a42
Plusb Preco <plusb21@gmail.com>
2017-02-04 19:05:36 +0900

    Add test suite

---
+++ test/spec.js
@@ -0,0 +1,11 @@
+import {resolve} from 'path';
+import Jimp from 'jimp';
+import mergeImg from '../src';
+
+const fixturePath = resolve(__dirname, 'fixtures');
+
+test('`mergeImg()` returns `Promise` that contains `Jimp` object', async () => {
+  const image = await mergeImg([`${fixturePath}/example.png`, `${fixturePath}/example.png`]);
+
+  expect(image instanceof Jimp).toBeTruthy();
+});
